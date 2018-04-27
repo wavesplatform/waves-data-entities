5677d30ac7f07cddec7f8fc6577edcc19f2b6ca1
Ivan Shmidt <bahoc47@gmail.com>
2018-04-27 13:23:02 +0300

    Changed exports

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
-import { BigNumber } from './libs/bignumber';
-import { Asset } from './entities/Asset';
-import { Money } from './entities/Money';
-import { OrderPrice } from './entities/OrderPrice';
-import { AssetPair } from './entities/AssetPair';
-
-export { IAssetJSON } from './entities/Asset';
-export { IAssetPairJSON } from './entities/AssetPair';
-export { IMoneyJSON } from './entities/Money';
-export { IOrderPriceJSON } from './entities/OrderPrice';
-export default {
-  Asset,
-  Money,
-  OrderPrice,
-  BigNumber,
-  AssetPair,
-};
+export { BigNumber } from './libs/bignumber';
+export { Asset, IAssetJSON } from './entities/Asset';
+export { Money, IMoneyJSON } from './entities/Money';
+export { OrderPrice, IOrderPriceJSON } from './entities/OrderPrice';
+export { AssetPair, IAssetPairJSON } from './entities/AssetPair';
