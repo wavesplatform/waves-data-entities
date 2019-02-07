691c45ce90dc6eb63d6d15f8ccc39df6f387d44b
Jlewka <ua.lewka@gmail.com>
2019-02-07 19:21:49 +0300

    add Candle type; update Asset constructor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { BigNumber } from './libs/bignumber';
 export { Asset, IAssetJSON } from './entities/Asset';
+export { Candle, ICandleJSON } from './entities/Candle';
 export { Money, IMoneyJSON } from './entities/Money';
 export { OrderPrice, IOrderPriceJSON } from './entities/OrderPrice';
 export { AssetPair, IAssetPairJSON } from './entities/AssetPair';
