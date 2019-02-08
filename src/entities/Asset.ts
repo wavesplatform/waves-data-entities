cb5a939d75a0eabd52329f589a4d619b699ac4ed
Аleksej Petrov <apetrov@wavesplatform.com>
2019-02-08 12:38:36 +0300

    fix Asset type

--- src/entities/Asset.ts
+++ src/entities/Asset.ts
@@ -3,86 +3,86 @@ import { config } from '../config';
 import { toBigNumber } from '../utils';
 
 export interface IAssetInfo {
-  readonly ticker?: string;
-  readonly id: string;
-  readonly name: string;
-  readonly precision: number;
-  readonly description: string;
-  readonly height: number;
-  readonly timestamp: Date;
-  readonly sender: string;
-  readonly quantity: BigNumber | string | number;
-  readonly reissuable: boolean;
-  readonly hasScript: boolean;
-  readonly minSponsoredFee: BigNumber | string | number;
+    readonly ticker?: string;
+    readonly id: string;
+    readonly name: string;
+    readonly precision: number;
+    readonly description: string;
+    readonly height: number;
+    readonly timestamp: Date;
+    readonly sender: string;
+    readonly quantity: BigNumber | string | number;
+    readonly reissuable: boolean;
+    readonly hasScript?: boolean;
+    readonly minSponsoredFee?: BigNumber | string | number;
 }
 
 export interface IAssetJSON extends IAssetInfo {
-  readonly quantity: BigNumber;
-  readonly minSponsoredFee: BigNumber;
+    readonly quantity: BigNumber;
+    readonly minSponsoredFee?: BigNumber
 }
 
 export class Asset {
-  public readonly ticker: string | null;
+    public readonly ticker: string | null;
 
-  public readonly id: string;
-  public readonly name: string;
-  public readonly precision: number;
-  public readonly description: string;
+    public readonly id: string;
+    public readonly name: string;
+    public readonly precision: number;
+    public readonly description: string;
 
-  public readonly height: number;
-  public readonly timestamp: Date;
-  public readonly sender: string;
-  public readonly quantity: BigNumber;
-  public readonly reissuable: boolean;
-  public readonly hasScript: boolean;
-  public readonly minSponsoredFee: BigNumber;
+    public readonly height: number;
+    public readonly timestamp: Date;
+    public readonly sender: string;
+    public readonly quantity: BigNumber;
+    public readonly reissuable: boolean;
+    public readonly hasScript: boolean | null;
+    public readonly minSponsoredFee: BigNumber | null;
 
-  public readonly displayName: string;
+    public readonly displayName: string;
 
-  constructor(assetObject: IAssetInfo) {
-    assetObject = config.get('remapAsset')(assetObject);
+    constructor(assetObject: IAssetInfo) {
 
-    this.quantity = toBigNumber(assetObject.quantity);
-    this.minSponsoredFee = toBigNumber(assetObject.minSponsoredFee);
+        assetObject = config.get('remapAsset')(assetObject);
 
-    this.ticker = assetObject.ticker || null;
+        this.quantity = toBigNumber(assetObject.quantity);
+        this.minSponsoredFee = toBigNumber(assetObject.minSponsoredFee);
 
-    this.id = assetObject.id;
-    this.name = assetObject.name;
-    this.precision = assetObject.precision;
-    this.description = assetObject.description;
-    this.height = assetObject.height;
-    this.timestamp = assetObject.timestamp;
-    this.sender = assetObject.sender;
-    this.reissuable = assetObject.reissuable;
-    this.hasScript = assetObject.hasScript;
-    
-    this.displayName = assetObject.ticker || assetObject.name;
-  }
+        this.ticker = assetObject.ticker || null;
 
-  public toJSON(): IAssetJSON {
-    return {
-      ticker: this.ticker,
-      id: this.id,
-      name: this.name,
-      precision: this.precision,
-      description: this.description,
-      height: this.height,
-      timestamp: this.timestamp,
-      sender: this.sender,
-      quantity: this.quantity,
-      reissuable: this.reissuable,
-      hasScript: this.hasScript,
-      minSponsoredFee: this.minSponsoredFee,
-    };
-  }
+        this.id = assetObject.id;
+        this.name = assetObject.name;
+        this.precision = assetObject.precision;
+        this.description = assetObject.description;
+        this.height = assetObject.height;
+        this.timestamp = assetObject.timestamp;
+        this.sender = assetObject.sender;
+        this.reissuable = assetObject.reissuable;
+        this.hasScript = assetObject.hasScript || null;
+        this.displayName = assetObject.ticker || assetObject.name;
+    }
 
-  public toString(): string {
-    return this.id;
-  }
+    public toJSON(): IAssetJSON {
+        return {
+            ticker: this.ticker,
+            id: this.id,
+            name: this.name,
+            precision: this.precision,
+            description: this.description,
+            height: this.height,
+            timestamp: this.timestamp,
+            sender: this.sender,
+            quantity: this.quantity,
+            reissuable: this.reissuable,
+            hasScript: this.hasScript,
+            minSponsoredFee: this.minSponsoredFee
+        };
+    }
 
-  public static isAsset(object: object): object is Asset {
-    return object instanceof Asset;
-  }
+    public toString(): string {
+        return this.id;
+    }
+
+    public static isAsset(object: object): object is Asset {
+        return object instanceof Asset;
+    }
 }
